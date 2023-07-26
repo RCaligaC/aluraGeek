80cd740511638bde336ee03f8e3499146c4b0d2b
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2023-07-26 01:37:54 -0300

    JavaScript
    Ajustes responsivos finais de produtos e JavaScript da seção produtos da página principal

---
+++ components/cabecalho/cabecalho.js
@@ -0,0 +1,29 @@
+let cabeca = document.querySelector("[data-cabeca]");
+
+function criaCabecalho() {
+    const novoCabecalho = document.createElement("div");
+    novoCabecalho.className = "cabecalho";
+    novoCabecalho.innerHTML= `<nav class="cabecalho__nav">
+
+    <div class="cabecalho__nav__logo">
+        <a class="nav__logo" href="#"><img src="./assets/image/sports_esports_black_24dp 1.svg"
+                alt="Controle videogame"></a>
+        <a class="nav__texto" href="#"><img src="./assets/image/Texto.svg" alt="Texto Alura"></a>
+
+    </div>
+
+    <div class="cabecalho__nav__pesquisa">
+        <input class="cabecalho__pesquisa--input" placeholder="Pesquisar sobre..." type="text">
+        <img class="cabecalho__pesquisa__imgLupa" src="./assets/image/lupa.png" alt="Imagem de uma lupa">
+
+    </div>
+
+    </nav>
+
+    <a href="#" class="btn__login__cabecalho">Login</a>
+    <img class="cabecalho__imgLupa" src="./assets/image/lupa.png" alt="Imagem de uma lupa">`
+
+    return cabeca.appendChild(novoCabecalho);
+}
+
+/* criaCabecalho(); */
---
+++ services/mostrarProdutos.js
@@ -0,0 +1,48 @@
+/* passo 5 
+importar a função conectaApi do arquivo requisicaoProdutos.js*/
+import { conectaApi } from "./requisicaoProdutos.js";
+
+/* passo 6
+selecionar o elemento pai para criar o elemento filho com a estrutura HTML de cada video */
+const starwarsCards = document.querySelector("[data-starwars]");
+const consolesCards = document.querySelector("[data-consoles]");
+const diversosCards = document.querySelector("[data-diversos]");
+
+
+function constroiCard(imageUrl, name, price) {
+    const card = document.createElement("div");
+    card.className = "card__item";
+    card.innerHTML = `<img class="card__image" src="${imageUrl}" alt="imagem do produto">
+    <p class="card__nome">${name}</p>
+    <p class="card__preco">${price}</p>
+    <a class="btn__card" href="#">Ver produto</a>`
+
+    return card;
+}
+
+async function listaCard() {
+    const listaApi = await conectaApi.listaProdutos();
+
+    /* listaApi.forEach(element => cards.appendChild(constroiCard(element.imageUrl, element.name, element.price))); */
+
+    listaApi.forEach(element => {
+        
+        if (element.section == 'starWars') {
+
+            starwarsCards.appendChild(constroiCard(element.imageUrl, element.name, element.price));
+        }
+
+        if (element.section == 'consoles') {
+
+            consolesCards.appendChild(constroiCard(element.imageUrl, element.name, element.price));
+        }
+
+        if (element.section == 'diversos') {
+
+            diversosCards.appendChild(constroiCard(element.imageUrl, element.name, element.price));
+        }
+
+    });
+}
+
+listaCard();
